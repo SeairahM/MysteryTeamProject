3b06481fd41ed9384936e8930f2f5f316b789d50
dannamachi <serenita15@gmail.com>
2019-09-29 15:14:38 +1000

    debug add_sales p1

--- add_sales.php
+++ add_sales.php
@@ -16,19 +16,15 @@
   <p id="linesnum"><?php echo $linesnum; ?></p><p> items sold.</p>
 <?php
   //db connection - from dbconn.php
-  $DBServer = "localhost";
-  $DBUser = "admin";
-  $DBPass = "MysteryTeam2019";
-  $DB = "PHP";
-
-  $conn = mysqli_connect($DBServer, $DBUser, $DBPass, $DB);
+  require_once("dbconn.php");
+  $conn = $DBConn;
 
   //get lists of item names
   $sql = "SELECT itemID, itemName FROM Items";
   $result = $conn->query($sql);
+  echo "<p>". $result->num_rows. "rows</p>";
   //input sales form
-  echo "<form action=\"add_sales_process.php?submit=n\" method=\"POST\" id =\"form_process\"";
-  echo "<input type=\"text\" name=\"Payment Method\" />";
+  echo "<form action=\"add_sales_process.php?submit=n\" method=\"POST\" id =\"form_process\">";
   //hidden input of sale lines no
   echo "<input hidden type=\"text\" name=\"linesnum\" value=\"". $linesnum. "\" />";
   //iterate over lines of sales
@@ -37,17 +33,17 @@
     //select item from list
     $j = 0;
     echo "<label for=\"itemline_". $i. "\">Item ID</label>";
-    echo "<select name=\"itemID\" id=\"itemline_". $i. "\">";
+    echo "<select name=\"itemID_". $i. "\" id=\"itemline_". $i. "\">";
     while ($j < $result->num_rows) {
     //1 itemID per option
       while ($row = $result->fetch_assoc()) {
-          echo "<option value=\"". $row["itemID"]. ">". $row["itemID"]. "-". $row["itemName"]. "</option>";
-        }
+        echo "<option value=\"". $row["itemID"]. "\">". $row["itemID"]. "-". $row["itemName"]. "</option>";
+      }
       $j += 1;
     }
     echo "</select>";
     echo "<label for=\"amtline_". $i. "\">Item Amount</label>";
-    echo "<input type=\"text\" id=\"amtline_". $i. "\" name=\"itemAmount\" />";
+    echo "<input type=\"text\" id=\"amtline_". $i. "\" name=\"itemAmount_". $i. "\" />";
     $i += 1;
     //redo query for additional sale lines
     mysqli_free_result($result);
