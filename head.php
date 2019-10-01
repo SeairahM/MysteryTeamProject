27fed323cb8fe899ad2ff69886fd9a6653cb44c2
Bay101 <Bay101@users.noreply.github.com>
2019-10-01 22:11:06 +1000

    Fixed Stylesheet Reference
    Fixed Stylesheet Reference

--- head.php
+++ head.php
@@ -4,5 +4,5 @@
   echo "<meta name=\"description\" content=\"Great Pharmacy Stocktake\">";
   echo "<meta name=\"keywords\" content=\"Business\">";
   echo "<meta name=\"author\" content=\"Mystery Team\">";
-  echo "<link href=\"\" rel=\"stylesheet\">";
+  echo "<link href=\"PHP_SR_StyleSheet.css\" rel=\"stylesheet\">";
 ?>
