ea53f41260d03ae45417104324d18dc576b36c62
dannamachi <serenita15@gmail.com>
2019-10-01 22:24:43 +1000

    Update nav

--- navigation.php
+++ navigation.php
@@ -1,12 +1,11 @@
 <?php
 echo "<div id=\"navigation\">
   <nav>
-    <a href=\"index.php\">Sign in</a> |
-    <a href=\"signout.php\">Sign Out</a> |
-    <a href=\"addItemForm.php\">Add Item</a> |
     <a href=\"add_sales.php\">Add Sales</a> |
     <a href=\"saleDisplay.php\">View Sales</a> |
-    <a href=\"Sales.php\">Home Page</a> |
+    <a href=\"addItemForm.php\">Add Item</a> |
+    <a href=\"monthly_report.php\">Monthly Report</a> |
+    <a href=\"signout.php\">Sign Out</a> |
   </nav>
 </div>";
 ?>
