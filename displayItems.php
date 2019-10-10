89b64bf987a5a10598365361a81bdc93d69312ea
dannamachi <serenita15@gmail.com>
2019-10-10 20:17:39 +1100

    Display item description in table

--- displayItems.php
+++ displayItems.php
@@ -11,6 +11,12 @@
   <div>
   <h1>Items</h1>
   <?php
+  if (isset($_GET["edited"])) {
+    echo "<p>";
+    if ($_GET["edited"] == 'y') { echo "Item successfully updated."; }
+    else { echo "Something went wrong. Please try again."; }
+    echo "</p>";
+  }
   //function to echo edit button
   function echoEditButton($itemID) {
     echo "<form method=\"post\" action=\"editItem.php\">";
@@ -40,7 +46,7 @@
   if ($searchkey != "" || $searchkey != NULL) { $sql_search = " WHERE ". $searchitem. " = \"". $searchkey. "\" "; }
   else { $sql_search = " "; }
   $sql_order = $orderkey;
-  $sql = "SELECT itemID, itemName, stockAmt, categoryName, itemPrice
+  $sql = "SELECT itemID, itemName, itemNote, stockAmt, categoryName, itemPrice
   FROM Items i JOIN Categories c ON i". ".". "itemCategory = c". ".". "categoryID".
   $sql_search. $sql_arrange. $sql_order;
   // echo "<p>Query is: ". $sql;
@@ -50,7 +56,7 @@
   <form action="displayItems.php?query=y" method="POST">
     <!--search text input-->
     <label for='searchkey'>Search for:</label>
-    <input type='text' id='searchkey' name='searchkey' /><br />
+    <input type='text' id='searchkey' name='searchkey'/><br />
     <!--pick column to search-->
     <input type='radio' id='searchitem1' name='searchitem' value='itemID' />
     <label for='searchitem1'>by Item ID</label>
@@ -87,6 +93,7 @@
     <tr>
       <th>Item ID</th>
       <th>Item Name</th>
+      <th>Item Note</th>
       <th>Stock</th>
       <th>Category</th>
       <th>Unit Price</th>
@@ -97,6 +104,7 @@
       echo "<tr>";
       echo "<td>". $row["itemID"]. "</td>";
       echo "<td>". $row["itemName"]. "</td>";
+      echo "<td>". $row["itemNote"]. "</td>";
       echo "<td>". $row["stockAmt"]. "</td>";
       echo "<td>". $row["categoryName"]. "</td>";
       echo "<td>". $row["itemPrice"]. "</td>";
@@ -105,6 +113,7 @@
       echo "</td>";
       echo "</tr>";
     }
+    mysqli_free_result($result);
     ?>
   </table>
   </div>
