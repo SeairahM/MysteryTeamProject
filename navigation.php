cc49c0211568881c3b4316a04431b60c731430f2
dannamachi <serenita15@gmail.com>
2019-10-10 18:49:09 +1100

    Add display items func
    displayItems.php:
    - display all items
    - form to search/filter items
    - link to edit item form
    navigation.php:
    - fix nav logo error
    - link to displayItems.php

--- navigation.php
+++ navigation.php
@@ -1,13 +1,14 @@
 <?php
 echo "<header id=\"navigation\">
-		<img src=\"images/PHP_SR_Logo.png\" alt="Logo"/>
+		<img src=\"images/PHP_SR_Logo.png\" alt=\"Logo\"/>
 		<a href=\"signout.php\">SIGN-OUT</a>
 		<aside>
 		<nav>
 		    <ul>
 		      <li><a href=\"add_sales.php\">ADD SALES</a></li>
-		      <li><a href=\"saleDisplay.php\">DISPLAY SALES</a></li> 
+		      <li><a href=\"saleDisplay.php\">DISPLAY SALES</a></li>
 		      <li><a href=\"addItemForm.php\">ADD ITEMS</a></li>
+					<li><a href=\"displayItems.php\">DISPLAY ITEMS</a></li>
 		      <li><a href=\"monthly_report.php\">REPORTS</a></li>
 		    </ul>
 		</nav>
