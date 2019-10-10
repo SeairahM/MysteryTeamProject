f85e6f02f7e53257c2d6441ba5d4cccf90d945bc
Bay101 <Bay101@users.noreply.github.com>
2019-10-10 20:26:22 +1100

    Added deleteUser.php
    Added deleteUser.php with deleting users functionality

---
+++ deleteUser.php
@@ -0,0 +1,106 @@
+<?php
+require_once("checkLogin.php"); 
+require_once("dbconn.php");
+require_once("functions.php");
+
+//check to see if the page is being loaded as a result of submitting the form
+if(isset($_POST['username']))
+{
+    $username = sanitizeInput($_POST['username']);  
+
+     //check if username already exists
+     $query = "SELECT userID FROM Users WHERE username = '$username'";        
+     if ($result = mysqli_query($DBConn, $query)) {
+         $userExists = mysqli_num_rows($result);            
+         if ($userExists == 0){            
+            $errorMsg = "Invalid User";      
+            $displayError = TRUE;
+         }
+         else{
+            //username doesn't exist so create the user
+            $query = "DELETE FROM Users WHERE username='$username'";
+            if ($result = mysqli_query($DBConn, $query)) {
+                $displaySuccess = TRUE;
+            }
+            //Error Occured while adding user to database
+            else{
+                $displayError = TRUE;
+                $errorMsg = "Database Error";
+            }
+         }
+    }
+    //Error Occured checking if the user exists
+    else{
+        $displayError = TRUE;
+        $errorMsg = "Database Error";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>People's Health Pharmacy - Delete User</title>
+    <?php
+        include "head.php";
+    ?>
+</head>
+<body>
+	<header>
+		<img src="images/PHP_SR_logo.png" alt="Logo"/>
+		<a href="signout.php">SIGN-OUT</a>
+		<aside>
+        <?php
+            include "navigation.php";
+        ?>
+		</aside>
+	</header>
+
+	<div>
+        <?php         
+           if ($displayError)
+           {
+                echo "<div id=\"error\">
+                        <p>An Error Occured While Deleting User";
+                        if ($errorMsg != ""){
+                            echo " - " . $errorMsg;
+                        }
+                        echo "</p></div>";
+           }
+       ?>
+        <?php
+            if ($displaySuccess)
+            {   
+                echo "<div id=\"success\">
+                    <p>Successfully Deleted User</p>
+                </div>";
+            }
+        ?>
+	    <form action = "deleteUser.php" method = "POST">
+            <h1>Delete User</h1>
+            <label>Username:   </label>
+            <select name="username" required>
+            <?php
+                 $query = "SELECT username from Users";
+    
+                 if ($result = mysqli_query($DBConn, $query)) {                    
+                     while ($row = mysqli_fetch_assoc($result)) {
+                        $current = $row['username'];
+                        echo("<option value=\"$current\">$current</option>");
+                    }              
+                    
+                    mysqli_free_result($result);
+                }
+            ?>
+            </select><br/><br/>           
+            <input type="submit" value="Delete User"/><br/>
+        </form>
+	</div>
+
+	<footer>
+        <?php
+            include "footer.php";
+        ?>
+    	<section>
+	</footer>
+</body>
+</html>
